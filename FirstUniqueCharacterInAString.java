f0ec6f710f7e3efde584bbbebbdf209323c051eb
Akhilkumar31e <akhilkumar31e@gmail.com>
2020-05-05 12:58:25 +0530

    05 May

---
+++ FirstUniqueCharacterInAString.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int arr[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+        }
+        int i=0;
+        for(i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a']==1) return i;
+        }
+        return -1;
+    }
+}
