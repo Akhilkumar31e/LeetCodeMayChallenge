149c164b4c79cc9392d9a7baaa9c45bda19ee319
Akhilkumar31e <akhilkumar31e@gmail.com>
2020-05-17 22:30:20 +0530

    17 May

---
+++ Find-all-anagrams-in-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        if(s.length()<p.length()) return new ArrayList<Integer>();
+        long target=0;
+        long temp=0;
+        //Computing hash values for strings 
+        for(int i=0;i<p.length();i++){
+            target=target+(long)Math.pow(p.charAt(i),3);
+            temp=temp+(long)Math.pow(s.charAt(i),3);
+        }
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        if(temp==target) ans.add(0);
+        int len=p.length();
+       //Sliding window for string s
+        for(int i=1;i<s.length()-len+1;i++){
+            temp=temp-(long)Math.pow(s.charAt(i-1),3);
+            temp=temp+(long)Math.pow(s.charAt(i+len-1),3);
+            //Compare hash value 
+            if(temp==target) ans.add(i);
+        }
+        return ans;
+    }
+}
